2e7be700f3c305163a4996ef1fc2ba87491342b1
Polina Zelenskaya <pinkaiscrazy@gmail.com>
2025-08-09 01:38:49 +0300

    Moved javascript away from html

---
+++ static/js/room.js
@@ -0,0 +1,114 @@
+var socket;
+
+$(document).ready(function(){
+  socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + '/room');
+  socket.on('connect', function() {
+      socket.emit('join', {});
+  });
+  socket.on('disconnect', function(data) {
+    socket.emit('left', {});
+  });
+  socket.on('status', function(data) {
+      var shouldScroll = Math.ceil($('#chat')[0].scrollTop + $('#chat')[0].clientHeight) >= $('#chat')[0].scrollHeight;
+      $('#chat').append('<div class="chat-message">' + '&lt;' + data.msg + '&gt;' + '</div>');
+      if (shouldScroll) {
+          console.log("it should scroll");
+          $('#chat').scrollTop($('#chat')[0].scrollHeight);
+      }
+  });
+  socket.on('message', function(data) {
+      var shouldScroll = Math.ceil(10 + $('#chat')[0].scrollTop + $('#chat')[0].clientHeight) >= $('#chat')[0].scrollHeight;
+      var msg = '<div class="chat-message"><span class="username" style="color: ' + data.color + '">' + data.username + ': </span><span class="message">' + data.msg + '</span></div>'
+      $('#chat').append(msg);
+      if (shouldScroll) {
+          console.log("it should scroll");
+          $('#chat').scrollTop($('#chat')[0].scrollHeight);
+      }
+  });
+  socket.on('user-info', function(data) {
+      $('#user-submissions').empty();
+      $('#user-submissions').append('<h4 class="h4">~ Your guesses ~</h4>');
+      for (const submission of data["submissions"]) {
+          if (!submission["processed"]) {
+              $('#user-submissions').append('<div class="guess"><div class="guess_number number_unk">?</div><div class="guess_name">' + submission["song"] + '</div><div class="guess_person">' + submission["submitter"] + '</div></div>');
+          } else if (submission["score"] < 0.5) {
+              $('#user-submissions').append('<div class="guess"><div class="guess_number number_0">' + submission["score"] + '</div><div class="guess_name">' + submission["song"] + '</div><div class="guess_person">' + submission["submitter"] + '</div></div>');
+          } else if (submission["score"] < 1.5) {
+              $('#user-submissions').append('<div class="guess"><div class="guess_number number_1">' + submission["score"] + '</div><div class="guess_name">' + submission["song"] + '</div><div class="guess_person">' + submission["submitter"] + '</div></div>');
+          } else if (submission["score"] < 2.5) {
+              $('#user-submissions').append('<div class="guess"><div class="guess_number number_2">' + submission["score"] + '</div><div class="guess_name">' + submission["song"] + '</div><div class="guess_person">' + submission["submitter"] + '</div></div>');
+          } else {
+              $('#user-submissions').append('<div class="guess"><div class="guess_number number_3">' + submission["score"] + '</div><div class="guess_name">' + submission["song"] + '</div><div class="guess_person">' + submission["submitter"] + '</div></div>');
+          }
+      }
+      var submitBnt = document.getElementById("prediction-submit");
+      submitBnt.innerHTML = 'SEND <i>(' + data["submissions-left"] + ' Attempts left)</i>';
+  });
+  socket.on('score', function(data) {
+      $('#leaderboard').empty();
+      for (const user of data) {
+         var color = (user["is_online"]) ? "white" : "grey";
+         $('#leaderboard').append('<div class="person_div"><div class="person_name"><span class="username" style="color: ' + color + ';">' + user["username"] + '</span></div><div class="person_score">' + user["points"] + '</div></div>');
+         continue;
+      }
+  });
+  socket.on('result', function(data) {
+      result_type = data["type"];
+      result_val = data["result"];
+      if (result_type == "submitter") $('#submitter-score').html(result_val);
+      if (result_type == "author") $('#author-score').html(result_val);
+
+      console.log(data);
+  });
+  socket.on('stream-change', function(data) {
+      link = data["link"];
+      document.getElementById('stream').src = link;
+  });
+  socket.on('redirect', function(data) {
+      socket.disconnect();
+      // go back to the login page
+      window.location.href = "{{ url_for('index') }}";
+  });
+  $('#send').click(function(e) {
+      text = $('#text').val();
+      if (!text) return;
+      $('#text').val('');
+      socket.emit('text', {msg: text});
+  });
+  $('#prediction-submit').click(function(e) {
+      submitter = $('#submitter-input').val();
+      author = $('#author-input').val();
+      if (!submitter && !author) {
+          alert("At least submitter or an author must be guessed :3");
+          return;
+      };
+      $('#submitter-input').val('');
+      $('#author-input').val('');
+      if (!submitter) submitter = '';
+      if (!author) author = '';
+      socket.emit('prediction', {author: author, submitter: submitter});
+  });
+  $('#okak').click(function(e) {
+      socket.emit('text', {msg: 'okak'});
+  });
+  $('#wowie').click(function(e) {
+      socket.emit('text', {msg: 'wowie'});
+  });
+  $(this).find('input').keypress(function(e) {
+      // Enter pressed?
+      if(e.which == 10 || e.which == 13) {
+          text = $('#text').val();
+          if (!text) return;
+          $('#text').val('');
+          socket.emit('text', {msg: text});
+      }
+  });
+});
+
+function leave_room() {
+  socket.emit('left', {}, function() {
+      socket.disconnect();
+      // go back to the login page
+      window.location.href = "{{ url_for('index') }}";
+  });
+};
