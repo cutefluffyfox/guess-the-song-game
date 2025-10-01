02887652f1aa66912bed506f4564a6f2d0f20618
Polina Zelenskaya <pinkaiscrazy@gmail.com>
2025-10-01 22:45:32 +0300

    Changed submission count

--- scripts/game_rules.py
+++ scripts/game_rules.py
@@ -1,4 +1,4 @@
-MAX_SUBMISSION_COUNT = 3
+MAX_SUBMISSION_COUNT = 5
 
 POSSIBLE_SUBMITTERS = sorted([
     'CuteFluffyFox', 'yeouryl', 'Voice', 'Coconought', 'Potato', 'Ehtomit', 'Komider', 'Wizzet', 'SkyfallStar', 'Laya', 'Thires', 'AryaHimself', 'CocoBeansies', 'Dweamy'
