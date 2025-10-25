bfbce4b6b5c721aed4aba2a7e38b0bee7504cdc1
Le Voice <52206808+ElectroMad@users.noreply.github.com>
2025-10-25 18:22:06 +0200

    Update game_rules.py
    Guess the song for 26.10.2025

--- scripts/game_rules.py
+++ scripts/game_rules.py
@@ -1,17 +1,17 @@
 MAX_SUBMISSION_COUNT = 3
 
 POSSIBLE_SUBMITTERS = sorted([
-    'CuteFluffyFox', 'yeouryl', 'Voice', 'Coconought', 'Potato', 'Ehtomit', 'Komider', 'Wizzet', 'SkyfallStar', 'Laya', 'Thires', 'AryaHimself', 'Dweamy', 'Queen', 'CocoBeansies', 'Mask', 'Leonyardo'
+    'Ckcky’, ‘Komider’, ‘Yeo’, ‘Fluffy’, ‘Thires’, ‘Voice’, ‘SkyfallStar’, ‘Leonyardo’, ‘Potato’, ‘CocoBeansies’, ‘Laya’, ‘Wizzet’, ‘Coconaught’, ‘Ethomit’, ‘Arya’, ‘Queen’, ‘Thires’
 ])
 
 POSSIBLE_AUTHORS = sorted([
-    'Delta Goodrem', 'Camellia', 'JAMIL', 'YonKaGor', 'Aespa', 'Jamie Christopherson', 'Lovejoy', 'Kamelot', 'Vesyolye Rebyata', 'Sunrise Skater Kids', 'Mystery Skulls', 
-    'Chris Christodoulou', 'Mikado', 'Hideki Naganuma', 'The scary joke', 'Mitsukiyo', 'Tyler, the creator'
+    'Kn1ght', 'Glyde', 'Dad Feels', 'Exmortus', 'Metallica', 'Mariah Carrey', 'Nel Cicierega', 'Bring me the Horizon', 'Empire of a sun', 
+    'Natori', 'Starset', 'Beck ft Robyn & the lonely island', 'Parov Stelar', 'Glass beach', 'Jonathan Bree', 'Jude York'
 ])
 
 POSSIBLE_TITLES = sorted([
-    'La belette', 'Crystallised', 'The Rock City Boy', 'Believe again', 'Ain\'t nothing like a funky beat', 'Homeless Millenial', 'Darling, I', 'Linger in the rain', 
-    'For the last time', 'Freaking out', 'The green letter', 'Is that me ??', 'When the lights are down', 'Coalescence', 'It has to be this way', 'Hold on tight', 'With Rob as my witness'
+    'Something Memorable', 'Overdose', 'Cold Weather', 'Infinite', '72 Seasons', 'Catgroove', 'Moonlight Sonata', '300MB', 'You’re so cool', 
+    'All I want for christmas is you', 'Can’t be stopped', 'Kingslayer', 'Super cool', 'Walking on a dream', 'Monsters', 'Those were the days'
 ])
 
 # DEFAULT_IFRAME_LINK = 'https://sketchfab.com/models/befbb2c422fc4b92aecae8c5114967e9/embed?autostart=1&internal=1&tracking=0&ui_infos=0&ui_snapshots=1&ui_stop=0&ui_watermark=0'
